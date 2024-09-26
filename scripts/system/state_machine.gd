158635019b82ccca180ae7d71509ceb923a0909b
josie yee <joshuayeemusic@gmail.com>
2024-09-25 22:35:26 -0700

    added controller input plugin

---
+++ scripts/system/state_machine.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var current_state : State
+var states : Dictionary = {}
+
+func _ready() -> void:
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+
+func _process(delta: float) -> void:
+	if current_state:
+		current_state.update(delta)
+
+func _physics_process(delta: float) -> void:
+	if current_state:
+		current_state.physics_update(delta)
